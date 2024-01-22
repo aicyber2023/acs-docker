8c2e3c0f3ad18f8785b2f35d4133da7f233af58f
aicyber2023 <aicyber2023@aicyber.com>
2024-01-22 18:48:52 +0800

    dockercomepose update

---
+++ dockercompose/acs-web/config/customer-service-chat/url.ts
@@ -0,0 +1,4 @@
+export default {
+  baseURL: "http://127.0.0.1:28002/prod-api",
+  shareURL: "http://127.0.0.1:28001",
+};
---
+++ dockercompose/acs-web/config/customer-service-server/config.js
@@ -0,0 +1,10 @@
+window.cfg = {
+
+  // 后端服务地址
+  // baseUrl: "http://127.0.0.1:8080",
+  baseUrl: "http://127.0.0.1:28002/prod-api",
+
+  // 前端对话地址
+  // chatUrl: 'http://127.0.0.1:3000',
+  chatUrl: 'http://127.0.0.1:28001',
+}
